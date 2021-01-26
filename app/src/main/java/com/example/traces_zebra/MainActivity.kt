59fd17ef6fc00be33b7dd59874ed278e07ccb2d2
erisbg <erisbg.513@gmail.com>
2021-01-26 18:45:07 -0500

    *Implementacion api ZEBRA

--- app/src/main/java/com/example/traces_zebra/MainActivity.kt
+++ app/src/main/java/com/example/traces_zebra/MainActivity.kt
@@ -1,90 +1,112 @@
 package com.example.traces_zebra
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.os.SystemClock
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.zebra.sdk.comm.BluetoothConnection
 import com.zebra.sdk.comm.Connection
 import com.zebra.sdk.comm.ConnectionException
-import com.zebra.sdk.comm.TcpConnection
+import com.zebra.sdk.printer.SGD
+import com.zebra.sdk.printer.ZebraPrinter
 import com.zebra.sdk.printer.ZebraPrinterFactory
 import com.zebra.sdk.printer.ZebraPrinterLanguageUnknownException
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import java.util.concurrent.Delayed
+import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var connection: Connection? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         printButton.setOnClickListener {
-            printConfigLabelUsingDnsName("ZEBRA")
+            connect()
+        }
+
+        blouthooButton.setOnClickListener {
+
         }
     }
 
-    @Throws(ConnectionException::class)
-    private fun sendZplOverTcp(theIpAddress: String) {
-        // Instantiate connection for ZPL TCP port at given address
-        val thePrinterConn: Connection = TcpConnection(theIpAddress, TcpConnection.DEFAULT_ZPL_TCP_PORT)
+
+    fun connect() {
+        connection = BluetoothConnection("00:22:58:3C:3C:0D")
         try {
-            // Open the connection - physical connection is established here.
-            thePrinterConn.open()
+            connection?.open()
+            setStatus("Connected", Color.GREEN)
+            var printer: ZebraPrinter? = null
+            if (connection!!.isConnected) {
+                try {
+                    printer = ZebraPrinterFactory.getInstance(connection)
+                    val cod = "! U1 setvar \"device.languages\" \"zpl\"\r\n"
+                    val format = "^XA " + "^DFR:TEMPLATE.ZPL^FS " + "^FO25,25^A0N,50,50^FDName^FS " + "^FO25,75^A0N,50,50^FN1^FS " + "^FO25,125^A0N,50,50^FDSurname^FS " + "^FO25,175^A0N,50,50^FN2^FS " + "^XZ"
+                    //  connection?.write(convertExtendedAscii(cod))
+                    printer.sendCommand(cod)
+                    printer.calibrate()
+                    printer.sendCommand("^XA^FO20,20^A0N,25,25^FDThis is a ZPL test.^FS^XZ")
+                    //  connection?.write(format.toByteArray())
+                    val retriever = String(printer.retrieveFormatFromPrinter("R:TEMPLATE.ZPL"))
+                    val data = HashMap<Int, String>()
+                    data[1] = "GERALDIN"
+                    data[2] = "ARCHILA"
+                    printer.printStoredFormat("R:TEMPLATE.ZPL", data)
+                    Log.e("conecct", "si" + printer.printerControlLanguage)
+                    setStatus("Determining Printer Language", Color.YELLOW)
+                    val pl = SGD.GET("device.languages", connection)
+                    setStatus("Printer Language $retriever", Color.BLUE)
+                } catch (e: ConnectionException) {
+                    Log.e("conecct", "äca" + e.message)
+                    printer = null
+                } catch (e: ZebraPrinterLanguageUnknownException) {
+                    Log.e("conecct", "äca" + e.message)
+                    printer = null
 
-            // This example prints "This is a ZPL test." near the top of the label.
-            val zplData = "^XA^FO20,20^A0N,25,25^FDThis is a ZPL test.^FS^XZ"
-
-            // Send the data to printer as a byte array.
-            thePrinterConn.write(zplData.toByteArray())
+                }
+            }
         } catch (e: ConnectionException) {
-            // Handle communications error here.
-            e.printStackTrace()
+            Log.e("conecct", "äca" + e.message)
+        } catch (e: Exception) {
+            Log.e("conecct", "äca" + e.message)
         } finally {
-            // Close the connection to release resources.
-            thePrinterConn.close()
+            connection?.close()
         }
+
     }
 
-    @Throws(ConnectionException::class)
-    private fun sendCpclOverTcp(theIpAddress: String) {
-        // Instantiate connection for CPCL TCP port at given address
-        val thePrinterConn: Connection = TcpConnection(theIpAddress, TcpConnection.DEFAULT_CPCL_TCP_PORT)
-        try {
-            // Open the connection - physical connection is established here.
-            thePrinterConn.open()
-
-            // This example prints "This is a CPCL test." near the top of the label.
-            val cpclData = """
-        ! 0 200 200 210 1
-        TEXT 4 0 30 40 This is a CPCL test.
-        FORM
-        PRINT
-        
-        """.trimIndent()
-
-            // Send the data to printer as a byte array.
-            thePrinterConn.write(cpclData.toByteArray())
-        } catch (e: ConnectionException) {
-            // Handle communications error here.
-            e.printStackTrace()
-        } finally {
-            // Close the connection to release resources.
-            thePrinterConn.close()
+    fun convertExtendedAscii(input: String): ByteArray? {
+        val length = input.length
+        val retVal = ByteArray(length)
+        for (i in 0 until length) {
+            val c = input[i]
+            if (c.toInt() < 127) {
+                retVal[i] = c.toByte()
+            } else {
+                retVal[i] = (c.toInt() - 256).toByte()
+            }
         }
+        return retVal
     }
 
-    @Throws(ConnectionException::class)
-    private fun printConfigLabelUsingDnsName(dnsName: String) {
-        val connection: Connection = TcpConnection(dnsName, 9100)
-        try {
-            connection.open()
-            val p = ZebraPrinterFactory.getInstance(connection)
-            p.printConfigurationLabel()
-        } catch (e: ConnectionException) {
-            e.printStackTrace()
-        } catch (e: ZebraPrinterLanguageUnknownException) {
-            e.printStackTrace()
-        } finally {
-            // Close the connection to release resources.
-            connection.close()
-        }
+    private fun getMacAddressFieldText(): String {
+        //   return macAddressEditText.getText().toString()
+        return ""
     }
 
+    private fun setStatus(statusMessage: String, color: Int) {
+        Toast.makeText(applicationContext, statusMessage, Toast.LENGTH_LONG).show()
+//        runOnUiThread {
+//            statusField.setBackgroundColor(color)
+//            statusField.setText(statusMessage)
+//        }
+//        DemoSleeper.sleep(1000)
+    }
 }
